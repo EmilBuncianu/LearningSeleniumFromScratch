4841b4361e9ca0fa7a85f9b0dae0acc0e316b5c6
Emil Buncianu <emil.buncianu@ness.com>
2026-09-23 18:20:48 +0300

    fix(edge): elimina disable gpu

--- conftest.py
+++ conftest.py
@@ -42,10 +42,13 @@ def driver(request):
 
     elif browser_name == "firefox":
         options = webdriver.FirefoxOptions()
-        options.add_argument("-headless")
-        options.add_argument("-private")  # Echivalentul Incognito pentru Firefox
-        options.add_argument("--width=1920")
-        options.add_argument("--height=1080")
+        options.add_argument("--headless=new")   # Modul headless stabil
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--disable-blink-features=AutomationControlled")
+
+        options.add_argument("--no-sandbox")                # Ignoră restricțiile de sandbox din container
+        options.add_argument("--disable-dev-shm-usage")      # Previne prăbușirea din lipsă de memorie cache (/dev/shm)
+        options.add_argument("--disable-gpu")                # Dezactivează randarea grafică (esențial pe Linux Server)
 
         driver_instance = webdriver.Firefox(options=options)
 
